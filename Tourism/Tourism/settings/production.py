dceebde020f014b93d9be795a1e4931fdc09c04a
TN Sharma <sharma.tankanath@gmail.com>
2017-12-06 14:41:03 +0600

    adding production and local settings

--- Tourism/Tourism/settings/production.py
+++ Tourism/Tourism/settings/production.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'myproject',
         'USER': 'myprojectuser',
-        'PASSWORD': 'password',
+        'PASSWORD': 'mypassword',
         'HOST': 'localhost',
         'PORT': '',
     }
